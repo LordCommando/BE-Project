48a8c827372fbed2c0ea2c645ff57b29f8c6480c
LordCommando <ruzankp@gmail.com>
2019-03-09 16:07:39 +0530

    Form ready

--- signup.php
+++ signup.php
@@ -1,7 +1,37 @@
- <form action="signup2.php">
-  First name:<br>
-  <input type="text" name="firstname" value="Mickey"><br>
-  Last name:<br>
-  <input type="text" name="lastname" value="Mouse"><br><br>
+<!doctype html>
+<html>
+<head></head>
+<body>
+<table>
+	
+	<form method="post" action="signup2.php">
+  
+ <tr> <input type="text" name="firstname" placeholder="First Name" required><br><br>
+ 
+  <input type="text" name="lastname" placeholder="Last Name" required><br><br>
+ 
+  <input type="text" name="city" placeholder="City" required><br><br>
+  
+  <input type="text" name="taluka" placeholder="Taluka" required><br><br>
+ 
+  <input type="text" name="district" placeholder="District" required><br><br>
+  
+  <input type="text" name="state" placeholder="State" required><br><br>
+  
+  <input type="number" min="400000" max="499999"name="pincode" placeholder="Pincode" required><br><br>
+  
+  <input type="number" min="7000000000" max="9999999999"name="mobile" placeholder="Mobile" required><br><br>
+  
+  <input type="number" min="20000000" max="29999999"name="landline" placeholder="Landline"><br><br>
+  
+  <input type="email" name="email" placeholder="E-mail"><br><br>
+  
+  Photo:<br>
+  <input type="file" name="photo" placeholder="Photo"><br><br>
+
   <input type="submit" value="Submit">
+
 </form> 
+</table>
+</body>
+</html>
